ff4d2b8a5cc60805b8be76f7006481b44fc5daf8
Mirko <mirkoj1996@gmail.com>
2019-11-11 13:54:34 +0100

    add data model for list item

---
+++ app/src/main/java/com/example/android/basictodo/ListItem.java
@@ -0,0 +1,18 @@
+package com.example.android.basictodo;
+
+public class ListItem {
+
+    private String content;
+
+    public ListItem(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
